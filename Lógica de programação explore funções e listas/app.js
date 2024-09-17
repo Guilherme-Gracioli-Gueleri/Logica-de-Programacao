2d0676e1fa9458d7a4bca29c4460efd35e994cba
Guilherme Gueleri <gggueleri@gmail.com>
2024-09-17 12:12:12 -0300

    Programming logic: explore functions and lists

--- Lógica de programação explore funções e listas/app.js
+++ Lógica de programação explore funções e listas/app.js
@@ -1,4 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -8,9 +9,23 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do número secreto!');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+function verificarChute() { // criando uma função para a palavra "verificarChute"
+    let chute = document.querySelector('input').value; // criando uma variável chamada "chute" e nela está sendo armazenada o valor inserido pelo usuário no campo de texto feito lá no HTML
+    
+    if (chute == numeroSecreto) {  //Se chute for igual ao numeroSecreto fazer as instruções abaixo
+        exibirTextoNaTela('h1', 'Acertou!'); 
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; // operador ternário para caso a pessoa acerte de primeira aparecer TENTATIVA e não TENTATIVAS
+
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`; // variável criada para armazenar a frase com quantas tentativas foram feitas.
+        exibirTextoNaTela('p', mensagemTentativas);
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é MENOR!');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é MAIOR!');
+        }
+        tentativas++; 
+    }
 }
 
 function gerarNumeroAleatorio() {
